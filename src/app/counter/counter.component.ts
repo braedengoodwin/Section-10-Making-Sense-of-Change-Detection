4666863bf8614dfe0049fff77b6a4a618878189e
braedengoodwin <braedenagoodwin@hotmail.com>
2025-04-03 16:15:00 -0500

    done with this section

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,9 @@
-import { Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -8,13 +13,26 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   count = signal(0);
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    //concept known as 'avoid zone pollution'
+
+    setTimeout(() => {
+      console.log('Timer Expired!');
+    }, 5000);
   }
 
   onDecrement() {
